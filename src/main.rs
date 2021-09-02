b6554fa4ecc56e6a8479dbf74f2c48b9616b5d76
Peter Verswyvelen <bugfact@gmail.com>
2021-09-02 18:56:24 +0200

    Playing around

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::time::Duration;
+
+use ftp::FtpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let mut ftp_stream = FtpStream::connect("127.0.0.1:21").unwrap();
+    println!("Connected!");
+    ftp_stream.get_ref().set_read_timeout(Some(Duration::from_secs(10)))
+                .expect("set_read_timeout call failed");
+    ftp_stream.login("Doe", "mumble").unwrap();
+    println!("Logged in!");
+    let cwd = ftp_stream.pwd().unwrap();
+    println!("CD: {:?}", cwd);
+    let list = ftp_stream.nlst(Some(&cwd)).unwrap();
+    println!("Dir: {:?}!", &list);
+    let _ = ftp_stream.quit();
 }
