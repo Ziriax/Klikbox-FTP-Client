acdb8d9aaf4a71b11e11d5ef645a679d337eb71c
Peter Verswyvelen <bugfact@gmail.com>
2021-09-12 02:53:51 +0200

    Completed Rust prototype too. Is fun!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::fmt::Debug;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 use std::thread;
 use std::time::Duration;
 use std::{error::Error, fs};
 
 use confy::{self};
-use ftp::FtpStream;
+use ftp::{FtpError, FtpStream};
 use serde::{Deserialize, Serialize};
 
 static CONFIG_FILE_NAME: &str = "klikbox-ftp-client";
@@ -24,8 +27,8 @@ impl ::std::default::Default for Config {
     fn default() -> Self {
         Self {
             ip: "127.0.0.1:21".to_string(),
-            src: "/".to_string(),
-            dst: "c:\\temp".to_string(),
+            src: "/home/ftp/".to_string(),
+            dst: "c:\\temp\\photos".to_string(),
             user: "Doe".to_string(),
             password: "mumble".to_string(),
         }
@@ -77,22 +80,73 @@ fn main() {
     // let _ = ftp_stream.quit();
 }
 
+fn download(ftp: &mut FtpStream, src_name: &str, dst_dir: &str) -> Res<()> {
+    let _ = ftp.retr(src_name, |stream| {
+        let mut buf = Vec::new();
+        stream
+            .read_to_end(&mut buf)
+            .and_then(|_| {
+                let src_path = Path::new(src_name);
+                let filename = src_path.file_name().unwrap();
+                let dir_path = Path::new(dst_dir);
+                let dst_path = dir_path.join(filename);
+                println!("Creating file {:?}", dst_path);
+                File::create(dst_path).and_then(|mut file| file.write(&buf))
+            })
+            .map_err(|e| FtpError::ConnectionError(e))
+    })?;
+
+    ftp.rm(src_name)?;
+
+    Ok(())
+}
+
 fn run() -> Res<()> {
     loop {
         println!("Connecting...");
         let cfg = Config::load()?;
         let mut ftp = FtpStream::connect(cfg.ip)?;
+        let tcp = ftp.get_ref();
+        tcp.set_read_timeout(Some(Duration::from_secs(5)))?;
+
         ftp.login(cfg.user.as_str(), cfg.password.as_str())?;
-        ftp.cwd(cfg.src.as_str())?;
-        fs::create_dir_all(cfg.dst)?;
+
+        let dst = cfg.dst.as_str();
+        fs::create_dir_all(dst)?;
 
         println!("Retrieving listing...");
         // List all files in the FTP source directory
-        let list = ftp.list(None)?;
-        let lines = list.join("\n");
-        println!("{}", lines.as_str());
+        let list = ftp.nlst(Some(cfg.src.as_str()))?;
+        // let lines = list.join("\n");
+        // println!("{}", lines.as_str());
 
-        println!("Sleeping...");
-        thread::sleep(Duration::from_secs(1));
+        println!("Retrieving time stamps...");
+
+        let pairs: Vec<_> = list
+            .iter()
+            .flat_map(|path| match ftp.mdtm(path) {
+                Ok(otm) => otm.map(|date| (path, date)),
+                Err(_) => None,
+            })
+            .collect();
+
+        println!("Sleeping 5 seconds...");
+        thread::sleep(Duration::from_secs(5));
+
+        println!("Finding downloadable files...");
+
+        let ready: Vec<_> = pairs
+            .iter()
+            .flat_map(|(path, date)| match ftp.mdtm(path) {
+                Ok(otm) => otm.map(|mt| (*path, mt.eq(date))),
+                Err(_) => None,
+            })
+            .flat_map(|(path, stable)| if stable { Some(path) } else { None })
+            .collect();
+
+        for &path in ready.iter() {
+          println!("Downloading {}...", path);
+          download(&mut ftp, &path, cfg.dst.as_str())?;
+        }
     }
 }
